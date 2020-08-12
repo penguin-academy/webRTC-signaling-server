f310ca728ad1d112b8e21c32387a4d1a811937ca
Björn Schmidtke <bs@pa1nd.de>
2020-08-11 23:16:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# webRTC Signaling Server
+
+This is a minimalistic signaling server for webRTC using socket.IO.
+
+## Getting Started
+
+Make sure to have node v12 or later install the dependencies:
+
+```bash
+npm install
+# or
+yarn
+```
+
+Now, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+The server is now open on localhost:8000. You can now use the sever to connect with socket-io client and handle the signaling.
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+io.origins('*:*')
+
+let rooms = {}
+
+app.use(express.json())
+
+io.on('connection', socket => {
+  socket.on('disconnect', () => {
+    for (room in rooms) {
+      rooms[room] = rooms[room].filter(e => e != socket.id)
+    }
+  })
+
+  socket.on('start call', roomID => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id)
+      if (rooms[roomID].length > 2) {
+        rooms[roomID] = []
+        rooms[roomID].push(socket.id)
+      } else if (rooms[roomID].length > 1) {
+        let hostID = rooms[roomID][0]
+        let partnerID = rooms[roomID][1]
+
+        io.to(hostID).emit('call partner', partnerID)
+        io.to(partnerID).emit('call host', hostID)
+      }
+    } else {
+      rooms[roomID] = [socket.id]
+    }
+  })
+  socket.on('offer', payload => {
+    io.to(payload.target).emit('offer', payload)
+  })
+
+  socket.on('answer', payload => {
+    io.to(payload.target).emit('answer', payload)
+  })
+
+  socket.on('ice-candidate', incoming => {
+    io.to(incoming.target).emit('ice-candidate', incoming.candidate)
+  })
+})
+
+const port = process.env.PORT || 8000
+server.listen(port, () => console.log(`server is running on port ${port}`))
